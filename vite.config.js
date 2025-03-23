a52d337b0d81927794d78d4601dd98af3281c5a8
Victor Cardoso Cunha <105755943+VictorCardosoOl@users.noreply.github.com>
2025-03-23 01:15:45 -0300

    att
    O sacrificio

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/',
+  root: resolve(__dirname, 'src'),
+  publicDir: resolve(__dirname, 'public'),
   build: {
+    outDir: resolve(__dirname, 'dist'),
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/pages/index.html'),
-        gallery: resolve(__dirname, 'src/pages/galeria.html')
+        main: resolve(__dirname, 'src/index.html'),
+        galeria: resolve(__dirname, 'src/pages/galeria1.html')
       }
     }
   },
